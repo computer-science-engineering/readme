43d5bb13ba820cb254490ec00b125801ac50cf16
Manas Talukdar <manas.talukdar@gmail.com>
2021-01-04 01:28:29 -0800

    WIP.

--- repositories.md
+++ repositories.md
@@ -1,3 +1,20 @@
 
-Repositories
-============
+dummy
+=====
+
+# Repositories
+
+## Coding
+  
+- [Learning Computer Science](https://github.com/computer-science-engineering/learning-computer-science)  
+
+## Reading
+
+### Book
+  
+- [Building Microservices -  Designing Fine-Grained Systems](https://github.com/computer-science-engineering/notes-book_bmdfgs)  
+- [Designing Data Intensive Applications](https://github.com/computer-science-engineering/notes-book_ddia)  
+- [Distributed systems for fun and profit](https://github.com/computer-science-engineering/notes-book_dsffp)  
+- [I Heart Logs: Event Data, Stream Processing, and Data Integration](https://github.com/computer-science-engineering/notes-book_i-heart-logs_jay-kreps)  
+- [Production-Ready Microservices](https://github.com/computer-science-engineering/notes-book_prm)  
+- [Scaling Teams: Strategies for Building Successful Teams and Organizations](https://github.com/computer-science-engineering/notes-book_scaling-teams_grosse_loftesness)  
--- src/update-repositories-md.py
+++ src/update-repositories-md.py
@@ -2,6 +2,8 @@ import os
 import json
 from mdutils.mdutils import MdUtils
 import numpy as np
+from itertools import groupby
+from operator import itemgetter
 
 
 def walk_max_depth(top, maxdepth):
@@ -52,10 +54,31 @@ def get_data(files):
 
 def create_file(files):
     data = get_data(files)
-    mdFile = MdUtils(file_name='repositories', title='Repositories')
-    sorted(data, key=lambda k: k['type'])
-    types = np.unique([d['type'] for d in data])
-    mdFile.create_md_file()
+    md_file = MdUtils(file_name='repositories', title='dummy')
+    md_file.new_header(level=1, title='Repositories')
+    grouped_by_type = groupby(data, key=itemgetter('type'))
+    for key, value in grouped_by_type:
+        md_file.new_header(level=2, title=key)
+        if key == 'Reading':
+            write_reading_entries(value, md_file)
+        for item in value:
+            write_item(item, md_file)
+        md_file.new_line()
+    md_file.create_md_file()
+
+
+def write_reading_entries(data, md_file):
+    grouped_by_sub_heading = groupby(data,
+                                     key=itemgetter('reading_sub_header'))
+    for key, value in grouped_by_sub_heading:
+        md_file.new_header(level=3, title=key)
+        for item in value:
+            write_item(item, md_file)
+
+
+def write_item(item, md_file):
+    md_file.new_line(
+        '- ' + md_file.new_inline_link(link=item['url'], text=item['name']))
 
 
 def get_reading_sub_header(file):
