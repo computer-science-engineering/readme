c63d54fed75f38741ad17645fbcdc92190d41e42
Manas Talukdar <manas.talukdar@gmail.com>
2021-01-09 23:35:33 -0800

    Updated for for list of repos to use submodules from lcs repo. #4

--- src/update_repositories_md.py
+++ src/update_repositories_md.py
@@ -1,9 +1,13 @@
 import os
 import json
+from itertools import chain
+import git
 from mdutils.mdutils import MdUtils
 import numpy as np
 from itertools import groupby
-from operator import itemgetter
+from operator import itemgetter, sub
+
+LCS_REPO_NAME = 'learning-computer-science'
 
 
 def walk_max_depth(top, maxdepth):
@@ -22,18 +26,34 @@ def find_files():
     result = {}
     root_dir = "./repositories"
     cwd = os.getcwd()
-    path_separator = os.sep
     #print(os.listdir(root_dir))
     for root, dirs, files in walk_max_depth(root_dir, 2):
         dirs.sort()
         for file in files:
             if file.endswith("metadata.json"):
-                metadatafile = os.path.normpath(os.path.join(
-                    cwd, file)).replace(path_separator, "/")
-                contents = open(metadatafile)
-                metadata = json.load(contents)
-                result[root] = (metadatafile, metadata)
+                metadatafile = os.path.normpath(os.path.join(cwd, file))
+                metadata_file = open(metadatafile)
+                metadata = json.load(metadata_file)
+                result[os.path.normpath(root)] = (metadatafile, metadata)
     return result
+
+
+def get_submodules(files):
+    submodules_result = {}
+    cwd = os.getcwd()
+    for key, value in files.items():
+        repo = git.Repo(key)
+        for submodule in repo.submodules:
+            path_to_submodule_part = os.path.normpath(
+                os.path.join(key, submodule.path))
+            path_to_metadata_file = os.path.normpath(
+                os.path.join(cwd, path_to_submodule_part, 'metadata.json'))
+            metadata_file = open(path_to_metadata_file)
+            metadata = json.load(metadata_file)
+            submodules_result[path_to_submodule_part] = (path_to_metadata_file,
+                                                         metadata)
+    return dict(
+        chain.from_iterable(d.items() for d in (files, submodules_result)))
 
 
 def get_data(files):
@@ -42,7 +62,7 @@ def get_data(files):
         data_dict = {}
         data_dict['type'] = value[1]['type']
         data_dict['name'] = value[1]['name']
-        local_path_parts = value[0].split('/')
+        local_path_parts = value[0].split(os.path.sep)
         repo_name = local_path_parts[-2]
         data_dict[
             'url'] = f'https://github.com/computer-science-engineering/{repo_name}'
@@ -58,10 +78,11 @@ def create_file(files):
     md_file.new_header(level=1, title='Repositories')
     grouped_by_type = groupby(data, key=itemgetter('type'))
     for key, value in grouped_by_type:
+        value_sorted = sorted(value, key=lambda x: x['name'])
         md_file.new_header(level=2, title=key)
         if key == 'Reading':
-            write_reading_entries(value, md_file)
-        for item in value:
+            write_reading_entries(value_sorted, md_file)
+        for item in value_sorted:
             write_item(item, md_file)
         md_file.new_line()
     md_file.create_md_file()
@@ -91,7 +112,8 @@ def get_reading_sub_header(file):
 def main():
     """main method."""
     files = find_files()
-    create_file(files)
+    files_including_submodules = get_submodules(files)
+    create_file(files_including_submodules)
 
 
 if __name__ == '__main__':
